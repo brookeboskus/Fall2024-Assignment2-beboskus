339d7abab7170654c51a223526e467417429c0f9
brookeboskus <beboskus@crimson.ua.edu>
2024-09-30 12:47:33 -0500

    comitting changes

--- Fall2024-Assignment2-beboskus/wwwroot/script.js
+++ Fall2024-Assignment2-beboskus/wwwroot/script.js
@@ -1,3 +1,5 @@
+//array of background images available in this project
+//this array can easily be added to if you upload a new image into the /wwwroot/Images directory and add it to this list
 const backgroundImg = [
     '/Images/image1.jpg',
     '/Images/image2.jpg',
@@ -6,6 +8,11 @@ const backgroundImg = [
     '/Images/image5.jpg'
 ];
 
+//globally set the first URL (this is used for the feeling lucky button)
+let firstResultURL = '';
+
+//most of this was given to us
+//I changed the Key and the URL being used to match what I used in postman to run this API
 function apiSearch() {
     var params = {
         'q': $('#query').val(),
@@ -24,6 +31,7 @@ function apiSearch() {
         .done(function (data) {
             var len = data.webPages.value.length;
             var results = '';
+
             for (i = 0; i < len; i++) {
                 results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
             }
@@ -33,49 +41,86 @@ function apiSearch() {
                 width: 900,
                 resizeable: false,
                 draggable: false,
+                height: 900,
+                position: relative,
+                top: 200
             });
         })
+
         .fail(function () {
             alert('error');
         });
 }
 
+//This is very similar to the apiSearch function
+//The idea is that the feeling lucky button can be clicked even before the search button is clicked
+//Therefore we have to call the API to query the first website that would display if we were to press the search button
+function feelingLucky() {
+    var params = {
+        'q': $('#query').val(),
+        'count': 50,
+        'offset': 0,
+        'mkt': 'en-us'
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        type: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': '4445a593eb8146748cd4214a5e37bc91'
+        }
+    })
+        .done(function (data) {
+
+            if (data.webPages && data.webPages.value && data.webPages.value.length > 0) {
+                firstResultURL = data.webPages.value[0].url;
+                console.log('website URL:' + firstResultURL);
+                window.location.href = firstResultURL; // Redirect to the first result URL
+
+            } else {
+                alert("No results found.");
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching serach results:", error);
+            alert("An error occured while fetching search results");
+        });
+}
+
+//basically an eventListener for buttons being clicked
 $(document).ready(function () {
 
     $('#search-button').on('click', function () {
+        console.log('search button was clicked');
         apiSearch();
+    });
 
-
+    $('#lucky-button').on('click', function () {
+        console.log('luck button was clicked');
+        feelingLucky();
     });
 });
 
-
-//let index = Math.floor(Math.random() * backgroundImg.length);
+//index of which picture we are iterating through
 let index = 0;
 
+//iterating through the background pictures (called when searchEngineTitle is clicked)
 function changeBackgroundImage() {
-    console.log("click was detected");
     index = (index + 1) % backgroundImg.length;
-
     document.body.style.backgroundImage = `url("${backgroundImg[index]}")`;
-
 }
 
+//set background image when page is loaded
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("content was loaded");
-    document.getElementById('searchEngineTitle').addEventListener('click', changeBackgroundImage());
-    
     document.body.style.backgroundImage = `url("${backgroundImg[index]}")`;
 });
 
-//setBackground();
-//changeBackgroundImage();
-document.getElementById('searchEngineTitle').addEventListener('click', changeBackgroundImage());
+//event listener for when earchEngineTitle is clicked
+$('#searchEngineTitle').on('click', function () {
+    changeBackgroundImage();
+});
 
-//$('#searchEngineTitle').on('click', function () {
-//    changeBackgroundImage();
-//});
-
+//function that gets the current time (called when time button is clicked)
 function getCurrentTime() {
     var currentDate = new Date();
     var hours = currentDate.getHours().toString().padStart(2, '0');
@@ -91,5 +136,5 @@ $('#time-button').on('click', function () {
     getCurrentTime();
 });
 
-$('#searchResults').show(); // or .fadeIn() for a smoother appearance
+$('#searchResults').show(); 
 $('#time').show();
